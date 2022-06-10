f86213cda4e54cba492f0d6e2cb75782353e3fa3
cganzon <crganzon@gmail.com>
2022-06-09 17:43:07 -0700

    format code

--- app.js
+++ app.js
@@ -7,29 +7,33 @@ const radioBtns = document.querySelectorAll("input[name='read-status']");
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-};
-
-function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-    console.log(myLibrary);
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-bookForm.addEventListener("submit", e => {
-    e.preventDefault();
-    let titleValue = titleInput.value;
-    let authorValue = authorInput.value;
-    let pagesValue = +pagesInput.value;
-    let isReadValue;
-    for(let radioBtn of radioBtns) {
-        if(radioBtn.checked) {
-            isReadValue = radioBtn.value;
-        }
-    }
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  console.log(myLibrary);
+}
 
-    addBookToLibrary(titleValue, authorValue, pagesValue, isReadValue === "read" ? true : false);
-})
+bookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let titleValue = titleInput.value;
+  let authorValue = authorInput.value;
+  let pagesValue = +pagesInput.value;
+  let isReadValue;
+  for (let radioBtn of radioBtns) {
+    if (radioBtn.checked) {
+      isReadValue = radioBtn.value;
+    }
+  }
+  addBookToLibrary(
+    titleValue,
+    authorValue,
+    pagesValue,
+    isReadValue === "read" ? true : false
+  );
+});
