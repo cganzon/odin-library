e6d2f94be35ad8fcf14893b2e3bcfb0af8104566
cganzon <crganzon@gmail.com>
2022-06-09 17:28:13 -0700

    change name and value of radio buttons. log form values in the console on form submit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const bookForm = document.querySelector("#book-form");
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
-const radioBtns = document.querySelectorAll("input[name='completed']");
+const radioBtns = document.querySelectorAll("input[name='read-status']");
 
 let myLibrary = [];
 
@@ -12,3 +12,20 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 };
+
+bookForm.addEventListener("submit", e => {
+    e.preventDefault();
+    let titleValue = titleInput.value;
+    let authorValue = authorInput.value;
+    let pagesValue = +pagesInput.value;
+    let isReadValue;
+    for(let radioBtn of radioBtns) {
+        if(radioBtn.checked) {
+            isReadValue = radioBtn.value;
+        }
+    }
+    console.log(titleValue)
+    console.log(authorValue)
+    console.log(pagesValue)
+    console.log(isReadValue)
+})
