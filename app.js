f3ca81341aea3637759570a0dc7b5eb4fbabe638
cganzon <crganzon@gmail.com>
2022-06-09 17:12:38 -0700

    add dom selectors for the form and inputs

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const bookForm = document.querySelector("#book-form");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const radioBtns = document.querySelectorAll("input[name='completed']");
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
