e2e260d27a19e7040ca1cf04803413f91af612ec
cganzon <crganzon@gmail.com>
2022-06-09 20:41:21 -0700

    add delete button to book card and change for of loop to regular for loop in order to access the index for the delete button

--- app.js
+++ app.js
@@ -30,17 +30,21 @@ function getSelectedRadioBtn(buttons) {
 
 function displayBooks(array) {
     booksContainer.textContent = "";
-    for(let book of array) {
+    for(let i = 0; i < array.length; i++) {
         const card = document.createElement("div");
         const bookTitle = document.createElement("h3");
         const bookAuthor = document.createElement("p");
         const bookPages = document.createElement("p");
         const bookReadStatus = document.createElement("p");
-        bookTitle.textContent = book.title;
-        bookAuthor.textContent = `By ${book.author}`;
-        bookPages.textContent = `${book.pages.toString()} pages`;
-        bookReadStatus.textContent = book.read ? "Completed" : "Not read yet";
-        card.append(bookTitle, bookAuthor, bookPages, bookReadStatus);
+        const deleteBtn = document.createElement("button");
+        
+        bookTitle.textContent = array[i].title;
+        bookAuthor.textContent = `By ${array[i].author}`;
+        bookPages.textContent = `${array[i].pages.toString()} pages`;
+        bookReadStatus.textContent = array[i].read ? "Completed" : "Not read yet";
+        deleteBtn.textContent = "X";
+        deleteBtn.setAttribute("data-num", i);
+        card.append(bookTitle, bookAuthor, bookPages, bookReadStatus, deleteBtn);
         booksContainer.append(card);
     }
 }
