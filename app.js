419305ac64cefe92e740ac869119e8570abea1e7
cganzon <crganzon@gmail.com>
2022-06-11 14:06:24 -0700

    move creating book card logic to a separate function

--- app.js
+++ app.js
@@ -32,36 +32,40 @@ function getSelectedRadioBtn(buttons) {
   }
 }
 
+function createBookCard(book, index) {
+  const card = document.createElement("div");
+  const bookTitle = document.createElement("h3");
+  const bookAuthor = document.createElement("p");
+  const bookPages = document.createElement("p");
+  const bookReadStatus = document.createElement("p");
+  const deleteBtn = document.createElement("button");
+  const readStatusBtn = document.createElement("button");
+
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = `By ${book.author}`;
+  bookPages.textContent = `${book.pages.toString()} pages`;
+  bookReadStatus.textContent = book.read ? "Completed" : "Not read yet";
+  deleteBtn.textContent = "X";
+  deleteBtn.setAttribute("data-num", index);
+  deleteBtn.classList.add("delete-btn");
+  readStatusBtn.textContent = "Change read status";
+  readStatusBtn.setAttribute("data-num", index);
+  readStatusBtn.classList.add("read-status-btn");
+  card.append(
+    bookTitle,
+    bookAuthor,
+    bookPages,
+    bookReadStatus,
+    deleteBtn,
+    readStatusBtn
+  );
+  booksContainer.append(card);
+}
+
 function displayBooks(array) {
   booksContainer.textContent = "";
   for (let i = 0; i < array.length; i++) {
-    const card = document.createElement("div");
-    const bookTitle = document.createElement("h3");
-    const bookAuthor = document.createElement("p");
-    const bookPages = document.createElement("p");
-    const bookReadStatus = document.createElement("p");
-    const deleteBtn = document.createElement("button");
-    const readStatusBtn = document.createElement("button");
-
-    bookTitle.textContent = array[i].title;
-    bookAuthor.textContent = `By ${array[i].author}`;
-    bookPages.textContent = `${array[i].pages.toString()} pages`;
-    bookReadStatus.textContent = array[i].read ? "Completed" : "Not read yet";
-    deleteBtn.textContent = "X";
-    deleteBtn.setAttribute("data-num", i);
-    deleteBtn.classList.add("delete-btn");
-    readStatusBtn.textContent = "Change read status";
-    readStatusBtn.setAttribute("data-num", i);
-    readStatusBtn.classList.add("read-status-btn");
-    card.append(
-      bookTitle,
-      bookAuthor,
-      bookPages,
-      bookReadStatus,
-      deleteBtn,
-      readStatusBtn
-    );
-    booksContainer.append(card);
+    createBookCard(array[i], i);
   }
 }
 
